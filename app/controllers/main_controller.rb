7f0aa2362439d26a63bdfdad739c67bfa57343ed
h4cker810 <harithmorgan123@gmail.com>
2015-02-28 18:50:32 -0500

    parenthesis

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -15,7 +15,7 @@ class MainController < ApplicationController
   	  #redirect_to( { :action => 'result', :answer => acceleration} )
   
 
-      final_velocity = params[ :initial_velocity ].to_f + params[:acceleration].to_f) * params[:time].to_f
+      final_velocity = params[ :initial_velocity ].to_f + params[:acceleration].to_f * params[:time].to_f
   	  redirect_to( { :action => 'result', :answer => final_velocity} )
 
   	#displacement = (params[ :initial_velocity ].to_f * params[:time].to_f)+(1/2)params[:acceleration].to_f
