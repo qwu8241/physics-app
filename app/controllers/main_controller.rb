b9a357b00751eaa2585a282290bc5e993ba56a8f
qwu8241 <qwu8241@bths.edu>
2015-02-28 18:23:03 -0500

    Revert "new"
    This reverts commit 15fb30a49ccf35b87cfae7a5bfe63498a06d2923.
    
    Conflicts:
    	app/controllers/main_controller.rb

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -13,11 +13,11 @@ class MainController < ApplicationController
   	acceleration = (params[ :final_velocity ].to_f - params[:initial_velocity].to_f)/params[:time].to_f
   	redirect_to( { :action => 'result', :answer => acceleration} )
 
-    final_velocity = params[ :initial_velocity ].to_f + params[:acceleration].to_f)*params[:time].to_f
-  	redirect_to( { :action => 'result', :answer => final_velocity} )
+   # final_velocity = params[ :initial_velocity ].to_f + params[:acceleration].to_f)*params[:time].to_f
+  	#redirect_to( { :action => 'result', :answer => final_velocity} )
 
-  	displacement = (params[ :initial_velocity ].to_f * params[:time].to_f)+(1/2)params[:acceleration].to_f
-  	redirect_to( { :action => 'result', :answer => displacement} )
+  	#displacement = (params[ :initial_velocity ].to_f * params[:time].to_f)+(1/2)params[:acceleration].to_f
+  	#redirect_to( { :action => 'result', :answer => displacement} )
 
   end
 
